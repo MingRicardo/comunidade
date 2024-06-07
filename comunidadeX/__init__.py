02d3016b3d414117c6d09e6bfede7cb930e2e233
Ricardo Ming <159853734+MingRicardo@users.noreply.github.com>
2024-06-07 17:53:51 -0300

    Update __init__.py

--- comunidadeX/__init__.py
+++ comunidadeX/__init__.py
@@ -2,16 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-import os
 
 app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = 'bce1308713443f50b0badc92a045387b'
-if os.getenv('DATABASE_URL'):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///comunidade.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidadeX.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
